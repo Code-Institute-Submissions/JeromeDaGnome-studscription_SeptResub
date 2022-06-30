484d4ab8623f3f7381a8de0857850dd9b2facf5a
JeromeDaGnome <jeremyitprof@gmail.com>
2022-06-30 07:52:34 +0000

    DEBUG

--- studscription/settings.py
+++ studscription/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 DEBUG = True
 
-#ALLOWED_HOSTS = ['studscription.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['studscription.herokuapp.com', 'localhost']
 
 
 # Application definition
