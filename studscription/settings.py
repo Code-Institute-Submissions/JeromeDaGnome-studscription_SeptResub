6e488bf7a60f904ea004ee295e2256573eac2fcc
JeromeDaGnome <jeremyitprof@gmail.com>
2022-07-17 17:27:04 +0000

    debugging static files

--- studscription/settings.py
+++ studscription/settings.py
@@ -172,9 +172,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+AWS_ACCESS_KEY_ID = 'AKIAYFLHVSMV3ZYW6SGI'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'studscription'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_DEFAULT_ACL = None
+
+AWS_LOCATION = 'static'
+STATICFILE_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
