96ec94778048f3e8138f4c57fa96a67ae6ca12d3
JeromeDaGnome <jeremyitprof@gmail.com>
2022-07-26 03:39:25 +0000

    testing settings debug

--- studscription/settings.py
+++ studscription/settings.py
@@ -174,7 +174,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-USE_AWS = os.getenv('USE_AWS') == 'TRUE'
+# USE_AWS = os.getenv('USE_AWS') == 'TRUE'
+USE_AWS = 'TRUE'
 
 if 'USE_AWS':
 
