f125b8949ca9989196ab099d72945ebf7252a3a4
JeromeDaGnome <jeremyitprof@gmail.com>
2022-07-29 01:54:40 +0000

    adding rating system to product detail page

---
+++ rating/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import BoxReview
+
+
+class AddReviewForm(forms.ModelForm):
+    """
+    Creates Add review form
+    """
+    class Meta:
+        """
+        Get model and add labels and widgets to the fields
+        """
+        model = BoxReview
+        fields = ('review_text', 'review_rating')
+        labels = {
+            "review_text": "Review",
+            "review_rating": "Rating"
+        }
+        widgets = {
+            'review_text': forms.Textarea(attrs={'rows': 3})
+            }
---
+++ rating/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from products import views
+
+urlpatterns = [
+    path("edit_review/<int:pk>/", views.edit_review, name="edit_review"),
+    path("delete_review/<int:pk>/", views.delete_review, name="delete_review"),
+]
